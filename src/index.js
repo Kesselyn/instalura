686d5ff51a2b44239838ba9da9c72f1867143d30
Kesselyn Fortunato <kesselyn.fortunato@maquinadobem.com>
2019-11-11 10:49:15 -0300

    Fim da a tividade 3 aula 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Login from './components/Login';
+import {Router, Route, browserHistory} from 'react-router';
 import './css/reset.css';
 import './css/timeline.css';
 import './css/login.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <App />, 
+  (
+    <Router history={browserHistory}>
+      <Route path="/" component={Login}/>
+      <Route path="/timeline" component={App}/>
+    </Router>
+  ), 
   document.getElementById('root')
 );
 
