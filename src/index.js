fe28a1176a8a80a9e191f8b2fe9697f74f3df712
Kesselyn Fortunato <kesselyn.fortunato@maquinadobem.com>
2019-11-11 12:15:31 -0300

    Fim da atividade 1 aula 4

--- src/index.js
+++ src/index.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Router, Route, browserHistory} from 'react-router';
+import {matchPattern} from 'react-router/lib/PatternUtils';
 import App from './App';
 import Login from './components/Login';
 import Logout from './components/Logout';
-import {Router, Route, browserHistory} from 'react-router';
 import './css/reset.css';
 import './css/timeline.css';
 import './css/login.css';
 import * as serviceWorker from './serviceWorker';
 
 function verificaAutenticacao(nextState, replace) {
-  if(localStorage.getItem('auth-token') === null) {
+  const result =  matchPattern('/timeline(/:login)', nextState.location.pathname);
+  const endercoPrivadoTimeLine = result.paramValues[0] === undefined;
+  if(endercoPrivadoTimeLine && localStorage.getItem('auth-token') === null) {
     replace('/?msg=Você precisa estar logado para acessar o endereço');
   }
 }
@@ -19,7 +22,7 @@ ReactDOM.render(
   (
     <Router history={browserHistory}>
       <Route path="/" component={Login}/>
-      <Route path="/timeline" component={App} onEnter={verificaAutenticacao}/>
+      <Route path="/timeline(/:login)" component={App} onEnter={verificaAutenticacao}/>
       <Route path="/logout" component={Logout}/>
     </Router>
   ), 
