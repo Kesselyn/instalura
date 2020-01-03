777504731ac619a961c118cf5c9fbbe67a66bed5
Kesselyn Fortunato <kesselyn.fortunato@maquinadobem.com>
2020-01-03 13:29:29 -0300

    Fim da atividade 2 aula 5

--- src/components/Foto.js
+++ src/components/Foto.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Link} from 'react-router';
+import Pubsub from 'pubsub-js';
 
 class HeaderFoto extends Component {
   render() {
@@ -20,13 +21,36 @@ class HeaderFoto extends Component {
 }
 
 class InformationFoto extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {likers : this.props.foto.likers};
+  }
+  
+  componentWillMount() {
+    Pubsub.subscribe('atualiza-liker', (topico, infoLiker) => {
+      if(this.props.foto.id === infoLiker.fotoId) {
+        const possivelLiker = this.state.likers.find(liker => liker.login === infoLiker.liker.login);
+        
+        if(possivelLiker === undefined) {
+          const novosLikers = this.state.likers.concat(infoLiker.liker);
+          this.setState({likers: novosLikers});
+          
+        } else {
+          const novosLikers = this.state.likers.filter(liker => liker.login !== infoLiker.liker.login);
+          this.setState({likers: novosLikers});
+        }
+
+      }
+    });
+  }
+
   render() {
     return(
       <div className="foto-info">
         <div className="foto-info-likes">
           {
-            this.props.foto.likers.map(liker => {
-              return <Link to={`/timeline/${liker.login}`} key={liker.login}>{liker.login},</Link>
+            this.state.likers.map(liker => {
+              return <Link to={`/timeline/${liker.login}`} key={liker.login}>{liker.login}, </Link>
             })
           }
 
@@ -73,6 +97,7 @@ class UpdateFoto extends Component {
       })
       .then(liker => {
         this.setState({likeada : !this.state.likeada});
+        Pubsub.publish('atualiza-liker', {fotoId: this.props.foto.id ,liker});
       });
   }
 
